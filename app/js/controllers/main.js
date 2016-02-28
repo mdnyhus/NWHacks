e33d90db11e46b687497119bed99d431eaea80e4
Corey Cole <coreyleoc@gmail.com>
2016-02-27 17:14:05 -0800

    angular http request working

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -5,8 +5,23 @@
  */
 angular.module('EventApp')
     .controller('MainCtrl', function ($scope, $http, eventfulKey) {
-        $http.get('http://api.eventful.com/rest/events/search?app_key=' + eventfulKey + 'keywords=books&location=San+Diego&date=Future')
-            .then(function(res) {
-                console.log(res);
-            });
+        //$http.get('http://api.eventful.com/rest/events/search?app_key=' + eventfulKey +
+        //    '&q=music&where=San%20Diego' + '' +
+        //    '&date=2015061000-2016062000&include=tags%2Ccategories&page_size=5&sort_order=popularity')
+        //    .then(function(res) {
+        //        $scope.data = res.data;
+        //    });
+
+        $http({
+            method: 'GET',
+            url: 'http://api.eventful.com/rest/events/search',
+            params: {
+                app_key: eventfulKey,
+                q: 'music',
+                where: 'San Diego',
+                date: '2015061000-2016062000'
+            }
+        }).then(function(res) {
+            $scope.data = res;
+        });
     });
