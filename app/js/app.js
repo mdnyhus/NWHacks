69a11a095dfddda0d1b92bd258e76283a5919af0
Khurram Ali <khurram.ali13d@gmail.com>
2016-02-27 16:54:07 -0800

    Added yelp api key.

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,7 @@ angular.module('EventApp', [
 ])
 .constant('firebaseUrl', 'https://nwhacks2016.firebaseio.com/')
 .constant('eventfulApiKey', 'wKZhJ3S2hDDLHtD5')
+.constant('yelpApiKey', 'SoLwK5jLDVspCB1x1wkKnA')
 .factory('rootRef', function(firebaseUrl) {
     return new Firebase(firebaseUrl);
 });
