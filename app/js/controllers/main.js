ad1d7429e4cb32be103133e3a6a2c0584ad38a93
mdnyhus <mdnyhus@gmail.com>
2016-02-27 19:54:00 -0800

    added location and city information

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -6,7 +6,29 @@
 angular.module('EventApp')
     .controller('MainCtrl', function ($scope, $http, eventfulKey) {
         $scope.chosenDate = new Date();
-
+        $scope.location = "";
+        $scope.city = "";
+        
+        window.onload = function() {
+          var input = document.getElementById('pac-input');
+          var autocomplete = new google.maps.places.Autocomplete(input);
+          google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            var place = autocomplete.getPlace();
+            $scope.location = place.formatted_address;
+            var components=this.getPlace().address_components,city='n/a';
+            if(components){
+              for(var c=0; c<components.length; c++){
+              console.log(components[c].types.join('|'))
+                if(components[c].types.indexOf('locality') > -1 && components[c].types.indexOf('political') > -1){
+                  city=components[c].long_name;
+                  break;
+                }
+              }
+              $scope.city = city;
+            }
+          });
+        }
+        
         $http({
             method: 'GET',
             url: 'http://api.eventful.com/json/events/search',
@@ -21,7 +43,10 @@ angular.module('EventApp')
         });
     });
     
-window.onload = function() {
-  var input = document.getElementById('pac-input');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-}
+// window.onload = function() {
+  
+  // autocomplete.addListener('place_changed', function() {
+    // window.alert($scope.location);
+          // return;
+  // });
+// }
